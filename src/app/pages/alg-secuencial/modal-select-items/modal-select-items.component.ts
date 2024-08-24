942c49b920011ff3f958282f54d094ebf3830305
Pana33 <62369981+Pana33@users.noreply.github.com>
2024-08-24 02:36:56 -0600

    Feat modal items
    Se crea la funcionalidad y el estilo del modal que se utilizara para modificar el array, solo falta poder seleccionar los objetos de uno a uno

--- src/app/pages/alg-secuencial/modal-select-items/modal-select-items.component.ts
+++ src/app/pages/alg-secuencial/modal-select-items/modal-select-items.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CardItemsComponent } from '../../../shared/components/card-items/card-items.component';
+import { ItemsInArrayComponent } from '../../../shared/components/items-in-array/items-in-array.component';
 
 @Component({
   selector: 'app-modal-select-items',
   standalone: true,
-  imports: [],
+  imports: [CardItemsComponent, ItemsInArrayComponent],
   templateUrl: './modal-select-items.component.html',
   styleUrl: './modal-select-items.component.scss'
 })
 export class ModalSelectItemsComponent {
+  @Input() itemsSelected:string[] = []
+  @Output() newItems = new EventEmitter<string[]>
+  copyOfItemsSelected:string[] = []
+  allItems:string[] = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'air-plane',
+    'android',
+    'balloon',
+    'bug',
+    'bus',
+    'cake',
+    'camera',
+    'clock',
+    'cloud',
+    'clover',
+    'droplet',
+    'letter',
+    'moon',
+    'pencil',
+    'phone',
+    'piggy',
+    'printer',
+    'rocket',
+    'shield',
+    'snowflake',
+    'star',
+    'suitcase',
+    'sun',
+    'trash',
+    'trophy',
+    'twitter',
+    'umbrella',
+    'windows',
+    'xbox',
+    'ying-yang',
+  ]
+
+  deleteItems(){
+    this.copyOfItemsSelected = []
+  }
+
+  addFiveRandomItems(){
+    for(let i = 0; i < 5; i++){
+      let randomNumer = Math.floor(Math.random() * 40)
+      this.copyOfItemsSelected.push(this.allItems[randomNumer])
+    }
+  }
+
+  cancelChanges(){
+    this.copyOfItemsSelected = []
+  }
+
+  applyChanges(){
+    if(this.copyOfItemsSelected.length === 0){
+      return
+    }
+    this.newItems.emit(this.copyOfItemsSelected)
+  }
+
+  createCopyOfListItems(){
+    this.copyOfItemsSelected = [...this.itemsSelected]
+  }
 
 }
