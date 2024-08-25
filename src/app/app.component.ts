3ce8f0fbea466e94898dd8d1b0f0ce998ee10bca
Pana33 <62369981+Pana33@users.noreply.github.com>
2024-08-24 21:04:56 -0600

    Style links to social media
    Se agrega un componente que envia a mis redes sociales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SocialMediaComponent } from './shared/components/social-media/social-media.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SocialMediaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
