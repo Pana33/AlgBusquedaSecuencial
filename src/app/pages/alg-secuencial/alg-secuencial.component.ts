c837bd0a1ad27e41c20c8eee75b578bec92069a7
Pana33 <62369981+Pana33@users.noreply.github.com>
2024-08-26 21:39:04 -0600

    Style pointer
    Se corrige un estilo del puntero que causaba que en dispositivos mobiles solo se mostrara la mitad de la barra y no todo el ancho

--- src/app/pages/alg-secuencial/alg-secuencial.component.ts
+++ src/app/pages/alg-secuencial/alg-secuencial.component.ts
@@ -94,7 +94,7 @@ export class AlgSecuencialComponent implements AfterViewInit {
     if(this.indexItemSelectedToFind === -1){
       return
     }
-    document.getElementById("pointerStep")!.style.top = this.currentStep === 0 ? '18px' : `${18 + this.currentStep * 24.25}px`
+    document.getElementById("pointerStep")!.style.top = this.currentStep === 0 ? '18px' : `${18 + this.currentStep * 24.1}px`
     switch(this.currentStep){
       case 0:
         this.callFunction()
